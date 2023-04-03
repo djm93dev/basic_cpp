315b4e6411cfd1590da17471e91be415755b6f15
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-03 13:49:26 -0400

    simple for loop

---
+++ Loops/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "Log.h"
+
+int main()
+{
+	for (int i = 0; i < 5; i++) // This is a simple for loop, it will run 5 times by incrementing i by 1 each time.
+	{
+		Log("Hello World!");
+	}
+	
+}
