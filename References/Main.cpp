903b9f528a0b1827410f36220638ee235063eaaf
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-03 14:44:56 -0400

    pass variable by reference

---
+++ References/Main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+// void Increment(int& value)
+//{
+//	value++;
+//}
+
+void Increment(int* value)
+{
+	(*value)++;
+}
+
+
+
+int main()
+{
+	int a = 5;
+	Increment(&a);
+	
+	std::cout << "a = " << a << std::endl;
+	std::cin.get();
+}
