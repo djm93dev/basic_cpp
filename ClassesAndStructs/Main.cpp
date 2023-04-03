7fbc965f51b0609d18cc6aa3d776726c1f7d1409
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-03 15:05:37 -0400

    classes vs structs

---
+++ ClassesAndStructs/Main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+class Player
+{
+public:
+	int x, y;
+	int speed = 1;
+
+	void Move(int xa, int ya)
+	{
+		x += xa * speed;
+		y += ya * speed;
+	}
+
+};
+
+
+int main()
+{
+	Player player;
+	player.x = 5;
+	player.y = 5;
+
+	player.Move(2, -2);
+
+	std::cout << player.x << ", " << player.y << std::endl;
+
+	std::cin.get();
+}
