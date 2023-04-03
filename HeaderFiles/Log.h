ad771eb7f698834de58ed8d420fe0b634d13ef50
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-03 11:53:58 -0400

    pragma once vs ifndef header gaurds

--- HeaderFiles/Log.h
+++ HeaderFiles/Log.h
@@ -1,4 +1,10 @@
-#pragma once // preprocesser directive to prevent multiple inclusions of this header file in the same compilation unit
+//#pragma once // preprocesser directive to prevent multiple inclusions of this header file in the same compilation unit
+
+#ifndef _LOG_H // if not defined
+#define _LOG_H // define
+
 
 void InitLog(); // delecration of the function InitLog
 void Log(const char* message); // declaration of the function Log
+
+#endif // end of the if not defined
