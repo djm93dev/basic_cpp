f2ad416993aa5ea0641709e4f57c7d84318f1626
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-03 11:04:30 -0400

    signed and unsigned ints

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -2,8 +2,11 @@
 
 int main()
 {
-	int variable = 8;
+	int variable = -2147483648; // int variable = the MIN value of an int
+	// unsigned int unsignedVariable = the MAX value of an unsigned int
+	unsigned int unsignedVariable = 4294967295;
 	
 	std::cout << variable << std::endl;
+	std::cout << unsignedVariable << std::endl;
 	std::cin.get();
 }
