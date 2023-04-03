c2f252a1083a4a89f97995cd3613668d670e5e70
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-03 11:14:08 -0400

    bools and ints for chars

--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -10,13 +10,17 @@ int main()
 	unsigned long unsignedLongVariable = 4294967295; // unsigned long unsignedLongVariable = the MAX value of an unsigned long
 	long long longLongVariable = 9223372036854775808; // long long longLongVariable = the MIN value of a long long
 	unsigned long long unsignedLongLongVariable = 18446744073709551615; // unsigned long long unsignedLongLongVariable = the MAX value of an unsigned long long
+	
+	
 	float floatVariable = 3.402823e+38f; // float floatVariable = the MAX value of a float
 	double doubleVariable = 1.7976931348623158e+308; // double doubleVariable = the MAX value of a double
 	long double longDoubleVariable = 1.7976931348623158e+308; // long double longDoubleVariable = the MAX value of a long double
 	
 	
 	
-	char character = 'a'; // char character = 'a'
+	char character = 65; // char character = 'A' or 65
+	
+	bool boolean = true; // bool boolean = true or false
 	
 
 	
@@ -32,5 +36,7 @@ int main()
 	std::cout << floatVariable << std::endl;
 	std::cout << doubleVariable << std::endl;
 	std::cout << longDoubleVariable << std::endl;
+	std::cout << character << std::endl;
+	std::cout << boolean << std::endl;
 	std::cin.get();
 }
