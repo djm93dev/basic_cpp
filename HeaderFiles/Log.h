4b60d748c7c632fa24d7f0b03daad8fbbef9e4cc
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-03 11:50:20 -0400

    header files

---
+++ HeaderFiles/Log.h
@@ -0,0 +1,4 @@
+#pragma once // preprocesser directive to prevent multiple inclusions of this header file in the same compilation unit
+
+void InitLog(); // delecration of the function InitLog
+void Log(const char* message); // declaration of the function Log
