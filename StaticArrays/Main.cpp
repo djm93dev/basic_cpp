acebb11c777fad5dc100d557564bc2d422cbeeb7
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-05 09:11:12 -0400

    static arrays

--- StaticArrays/Main.cpp
+++ StaticArrays/Main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
 #include <array>
 
+
+// pass in array without knowing the size
+void printArray(int array[], int size)
+{
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << array[i] << std::endl;
+	}
+}
+
+
 int main()
 {
 	std::array<int, 5> myArray = { 9, 7, 5, 3, 1 };
@@ -10,6 +21,9 @@ int main()
 	{
 		std::cout << myArray[i] << std::endl;
 	}
+
+	// Print the array using a function
+	printArray(myArray.data(), myArray.size());
 	
 	std::cin.get();
 }
