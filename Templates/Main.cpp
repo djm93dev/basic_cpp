1a9e353bff1d3e5739605c38e931d2c64a82c601
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-05 07:57:22 -0400

    templates

--- Templates/Main.cpp
+++ Templates/Main.cpp
@@ -8,11 +8,32 @@ void Print(T value)
 }
 
 
+template<typename T1, int N>
+class Array
+{
+private:
+	T1 m_Array[N];
+public:
+
+	// default constructor
+	Array()
+		: m_Array{ 0 }
+	{
+		std::cout << "Array created" << std::endl;
+	}
+	
+	int GetSize() const { return N; }
+};
+
+
 int main()
 {
 	Print(5);
 	Print("Hello");
 	Print(5.5f);
+	
+	Array<int, 5>array;
+	std::cout << array.GetSize() << std::endl;
 	
 	std::cin.get();
 }
