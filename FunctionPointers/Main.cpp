54476bc05eda8571ef43ff8fd648aba518c6c0c4
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-05 09:29:59 -0400

    function pointers

--- FunctionPointers/Main.cpp
+++ FunctionPointers/Main.cpp
@@ -1,4 +1,22 @@
 #include <iostream>
+#include <vector>
+
+
+//void PrintValue(int value)
+//{
+	//std::cout << "Value: " << value << std::endl;
+//}
+
+void ForEach(const std::vector<int>& v, void(*func)(int)) // func is a function pointer 
+{
+	for (int value : v)
+		func(value);
+}
+
+
+
+
+
 
 
 void Daniel()
@@ -8,6 +26,12 @@ void Daniel()
 
 int main()
 {
+
+	std::vector<int> v = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+	ForEach(v, [](int value) {std::cout << "Value: " << value << std::endl; }); // lambda expression
+
+
+	
 	typedef void(*func)();
 	func f1 = Daniel;
 	f1();
