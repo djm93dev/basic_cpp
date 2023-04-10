500b368714d31dc91ef779e386f8149ab4419e84
Daniel McDonald <daniel@danielmcdonald.dev>
2023-04-10 13:42:50 -0400

    Precompiled Headers

---
+++ PrecompiledHeaders/pch.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+#include <iostream>
+#include <algorithm>
+#include <functional>
+#include <memory>
+#include <thread>
+#include <memory>
+
+#include <string>
+#include <stack>
+#include <deque>
+#include <array>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+
+#include <Windows.h>
